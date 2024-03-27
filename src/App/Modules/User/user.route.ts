a9043aacc6e4db659147452ec5af8f46ae14c4d2
pervej0 <mdparvez222khan@gmail.com>
2024-03-27 22:17:57 +0600

    upload picture

--- src/App/Modules/User/user.route.ts
+++ src/App/Modules/User/user.route.ts
@@ -1,8 +1,9 @@
 import express from "express";
 import { createUser, getAllUser } from "./user.controller";
+import upload from "../../../shared/fileUpload";
 const router = express.Router();
 
-router.post("/", createUser);
+router.post("/", upload.single("file"), createUser);
 router.get("/", getAllUser);
 
 export const userRoute = router;
